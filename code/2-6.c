1dddf6d3ac2537419a5e9868945a88c985a86bd1
wanderer-ing <jsjune1222@gamil.com>
2026-05-28 21:31:09 +0900

    2.6

--- code/2-6.c
+++ code/2-6.c
@@ -8,7 +8,7 @@ int main(int argc, const char* argv[]) {
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
 
-    char buffer[100];
+    char buffer[1024];
     while (fgets(buffer,sizeof(buffer),fp)!=NULL){
         if(strstr(buffer,target_str)!=NULL){
             printf("%s",buffer);
